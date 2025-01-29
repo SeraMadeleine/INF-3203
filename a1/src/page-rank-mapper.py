6ae1690665615c003d4138f9c9982c0604c1b596
SeraMadeleine <sel063@uit.no>
2025-01-29 09:23:36 +0100

    added precode for task 1

---
+++ a1/src/page-rank-mapper.py
@@ -0,0 +1,5 @@
+def mapper(data):
+    raise NotImplementedError("This function is not yet implemented.")
+
+def reducer(data):
+    raise NotImplementedError("This function is not yet implemented.")
