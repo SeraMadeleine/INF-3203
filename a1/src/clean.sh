b8d1058f8912985e5b45d1515d7503d8aea944ae
SeraMadeleine <sel063@uit.no>
2025-02-24 09:55:32 +0100

    removed a1

--- a1/src/clean.sh
+++
@@ -1,1 +0,0 @@
-rm -rf mr_tmp/*
--- a1/src/page-rank-mapper.py
+++
@@ -1,5 +0,0 @@
-def mapper(data):
-    raise NotImplementedError("This function is not yet implemented.")
-
-def reducer(data):
-    raise NotImplementedError("This function is not yet implemented.")
